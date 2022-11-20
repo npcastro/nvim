f251c66238b24dc2432540a098ce1637b29df14e
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-11-20 09:33:49 -0300

    Add comments plugin

--- lua/npcastro/plugins.lua
+++ lua/npcastro/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Automatically close brackets
+  use "numToStr/Comment.nvim" -- Commenting plugin
 
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes to try out
@@ -79,6 +80,7 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use "p00f/nvim-ts-rainbow"
+  use "JoosepAlviste/nvim-ts-context-commentstring" -- Uses treesitter to set the type of commentstring based on the location of the cursor.
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
