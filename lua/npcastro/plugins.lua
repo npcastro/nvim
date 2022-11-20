d6a3e1778ac4c83422282e76115de7ee17f09ad5
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-11-20 03:28:08 -0300

    Add telescope

--- lua/npcastro/plugins.lua
+++ lua/npcastro/plugins.lua
@@ -69,6 +69,9 @@ return packer.startup(function(use)
   -- Snippets
   use "L3MON4D3/LuaSnip" --snippet engine
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
---
+++ lua/npcastro/telescope.lua
@@ -0,0 +1,47 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+
+    mappings = {
+      i = {   -- insert mode mappings
+        ["<CR>"] = actions.select_default,
+
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+      },
+
+      n = {   -- normal mode mappings
+        ["<CR>"] = actions.select_default,
+      },
+    },
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  },
+}
