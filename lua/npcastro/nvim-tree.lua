4b873faae36230fe621e041e3a8d61d26197581c
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-11-20 10:17:10 -0300

    Add nvim-tree

---
+++ lua/npcastro/nvim-tree.lua
@@ -0,0 +1,41 @@
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+-- to interact with the API
+-- local config_status_ok, nvim_tree_api = pcall(require, "nvim-tree-api.config")
+-- if not config_status_ok then
+--   return
+-- end
+
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'p', api.node.open.preview, opts('Open: Preview'))
+
+end
+
+nvim_tree.setup {
+  disable_netrw = true,
+  hijack_cursor = true,
+  diagnostics = {
+    enable = true,
+  },
+  on_attach = on_attach,
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
+  view = {
+    width = 40,
+  },
+}
+
