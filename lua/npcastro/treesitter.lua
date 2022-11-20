0bcee1c71dbd3639bc764f421925d3deaf7ec073
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-11-20 03:33:19 -0300

    Add treesitter

---
+++ lua/npcastro/treesitter.lua
@@ -0,0 +1,43 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = {
+    "bash",
+    "comment",
+    "css",
+    "dockerfile",
+    "gitignore",
+    "html",
+    "http",
+    "javascript",
+    "jsdoc",
+    "json",
+    "lua",
+    "make",
+    "markdown",
+    "python",
+    "regex",
+    "ruby",
+    "sql",
+    "typescript",
+    "vim",
+    "vue",
+    "yaml",
+  }, -- "all" or a map of languages
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = true,
+  },
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  -- indent = { enable = true, disable = { "yaml" } },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+  },
+  sync_install = false,
+}
