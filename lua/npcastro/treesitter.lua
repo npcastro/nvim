34a22746171f7142fceae9674d38df71742a6a59
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2023-08-27 01:03:31 -0400

    Treesitter changes

--- lua/npcastro/treesitter.lua
+++ lua/npcastro/treesitter.lua
@@ -4,6 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
+  auto_install = true,
   comment = {
     enable = true,
     enable_autocmd = false,
@@ -37,10 +38,8 @@ configs.setup {
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
   },
   ignore_install = { "" }, -- List of parsers to ignore installing
-  -- indent = { enable = true, disable = { "yaml" } },
   rainbow = {
     enable = true,
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
