0746e0f0be3c4e1786c63ec3ef62802d37597e08
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2023-08-27 20:30:53 -0400

    configure ruby formatting properly

--- lua/npcastro/lsp-zero.lua
+++ lua/npcastro/lsp-zero.lua
@@ -7,7 +7,6 @@ lsp.ensure_installed({
     'lua_ls',
     'pyright',
     -- rubocop must not be installed. Else it collides with the one on projects' gemfiles
-    'solargraph',
     'tsserver',
     'volar',
 })
@@ -17,42 +16,12 @@ lsp.nvim_workspace()
 
 vim.lsp.set_log_level("debug")
 
-
--- local null_ls_status_ok, null_ls = pcall(require, "null-ls")
--- if not null_ls_status_ok then
---   return
--- end
---
--- local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
---
--- null_ls.setup({
---   debug = false,
---   sources = {
---     -- javascript
---     formatting.prettier,
---     diagnostics.eslint,
---
---     -- lua
---     formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", 2 } }),
---
---     -- python
---     diagnostics.flake8,
---
---     -- ruby
---     formatting.rubocop,
---     diagnostics.rubocop,
---   },
--- })
-
 local lspconfig = require('lspconfig')
 
 lspconfig.rubocop.setup({})
--- lspconfig.solargraph.setup({})
 
 -- local jsonls_opts = require("npcastro.lsp.settings.jsonls")
 -- lspconfig["jsonls"].setup(vim.tbl_deep_extend("force", jsonls_opts, opts))
-
 
 lsp.set_sign_icons({
   error = '✘',
@@ -66,6 +35,7 @@ lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps(opts)
 
   vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.keymap.set('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
 end)
 
 lsp.setup()
