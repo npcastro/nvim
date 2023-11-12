f0506bdc215541aea7c941832e3155d47339463e
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2023-11-11 22:22:19 -0300

    add missing cmp icons

--- lua/npcastro/cmp.lua
+++ lua/npcastro/cmp.lua
@@ -7,12 +7,12 @@ end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "",
+  Text = "",
   Method = "m",
-  Function = "",
+  Function = "󰡱",
   Constructor = "",
   Field = "",
-  Variable = "",
+  Variable = "󰆧",
   Class = "",
   Interface = "",
   Module = "",
@@ -23,15 +23,15 @@ local kind_icons = {
   Keyword = "",
   Snippet = "",
   Color = "",
-  File = "",
+  File = "󰈙",
   Reference = "",
-  Folder = "",
+  Folder = "",
   EnumMember = "",
-  Constant = "",
+  Constant = "",
   Struct = "",
   Event = "",
-  Operator = "",
-  TypeParameter = "",
+  Operator = "",
+  TypeParameter = "󰊄",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
