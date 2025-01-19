a20bc86b55c5799ea3879dcbfda7136d768b3330
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2025-01-18 23:46:03 -0300

    add toggle supermaven command

--- lua/config/supermaven.lua
+++ lua/config/supermaven.lua
@@ -5,3 +5,22 @@ require('supermaven-nvim').setup({
     accept_word = "<C-l>",
   },
 })
+
+local toggle_supermaven = function()
+    local supermaven = require("supermaven-nvim")
+    local api = require("supermaven-nvim.api")
+    if supermaven.disable_inline_completion then
+      api.toggle()
+      print("Inline AI autocompletion ENABLED")
+    else
+      api.toggle()
+      print("Inline AI autocompletion DISABLED")
+    end
+  end
+
+vim.keymap.set(
+  'n',
+  '<leader>tia',
+  toggle_supermaven,
+  { desc = '[T]oggle [I]nline [A]I autocompletion' }
+)
