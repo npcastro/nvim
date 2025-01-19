aa07449a37b8429392b60f3961c322148f79f7b8
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2025-01-18 23:22:41 -0300

    remove lsp-zero

--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -1,46 +1,40 @@
-local lsp = require('lsp-zero').preset({})
-
-vim.lsp.set_log_level('WARN')
-
 local lspconfig = require('lspconfig')
+local lspconfig_defaults = lspconfig.util.default_config
 
-lspconfig.rubocop.setup({
-  filetypes = { "ruby", "rbi" },
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- vim.lsp.set_log_level('WARN')
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.keymap.set('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+  end,
 })
 
-lspconfig.sorbet.setup({
-  root_dir = lspconfig.util.root_pattern(".git"),
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '✘',
+      [vim.diagnostic.severity.WARN] = '▲',
+      [vim.diagnostic.severity.HINT] = '⚑',
+      [vim.diagnostic.severity.INFO] = '»',
+    },
+  },
 })
-
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }  -- avoid undefined global vim linter error
-      }
-    }
-  }
-})
-
--- local jsonls_opts = require("npcastro.lsp.settings.jsonls")
--- lspconfig["jsonls"].setup(vim.tbl_deep_extend("force", jsonls_opts, opts))
-
-lsp.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = '»'
-})
-
-lsp.on_attach(function(_, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  lsp.default_keymaps(opts)
-
-  vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.keymap.set('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
-end)
-
-lsp.setup_servers({'eslint', 'html', 'jsonls', 'lua_ls', 'pyright', 'volar'})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -56,33 +50,55 @@ require('mason-lspconfig').setup({
     'volar',
   },
   handlers = {
-    lsp.default_setup,
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+    sorbet = function()
+      lspconfig.sorbet.setup({
+        root_dir = lspconfig.util.root_pattern(".git"),
+      })
+    end,
+    rubocop = function()
+      lspconfig.rubocop.setup({
+        cmd = { os.getenv( "HOME" ) .. "/.rbenv/shims/rubocop", '--lsp' }, -- resolves collision between mason and gemfiles rubocop
+        filetypes = { "ruby", "rbi" },
+      })
+    end,
+    lua_ls = function()
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }  -- avoid undefined global vim linter error
+            }
+          }
+        }
+      })
+    end,
+    ts_ls = function()
+      local vue_typescript_plugin = require('mason-registry')
+      .get_package('vue-language-server')
+      :get_install_path()
+      .. '/node_modules/@vue/language-server'
+      .. '/node_modules/@vue/typescript-plugin'
+
+      lspconfig.ts_ls.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_typescript_plugin,
+              languages = {'javascript', 'typescript', 'vue'}
+            },
+          }
+        },
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+      })
+    end
   }
 })
-
-local vue_typescript_plugin = require('mason-registry')
-  .get_package('vue-language-server')
-  :get_install_path()
-  .. '/node_modules/@vue/language-server'
-  .. '/node_modules/@vue/typescript-plugin'
-
-lspconfig.ts_ls.setup({
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vue_typescript_plugin,
-        languages = {'javascript', 'typescript', 'vue'}
-      },
-    }
-  },
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
-})
-
-lspconfig.volar.setup({})
 
 vim.diagnostic.config({
   virtual_text = true,
   underline = true,
 })
-
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,9 @@
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" },             -- Required
-      { "williamboman/mason.nvim" },           -- Optional
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },     -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "L3MON4D3/LuaSnip" },     -- Required
-    }
-  },
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "L3MON4D3/LuaSnip" },
   { "mfussenegger/nvim-lint" },
 }
