f2edf890bdc5a984f6d7c32a391dd7fd27bc2b0a
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2025-10-29 17:26:59 -0300

    remove unused packages

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -1,13 +1,9 @@
 local npairs = require('nvim-autopairs')
 
 npairs.setup {
-  check_ts = true, -- use treesitter to check for pairs
+  check_ts = true,
   ts_config = {
     lua = { "string", "source" },
     javascript = { "string", "template_string" },
   },
 }
-
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_config = require('cmp')
-cmp_config.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })  -- add parenthesis after select on cmp
